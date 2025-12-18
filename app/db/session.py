888dc7b8f1f1b9a15b25973421f34773f74c30cf
Amar Dongardive <adongardive@ACMDC.com>
2025-12-18 17:00:20 +0530

    update sql code

--- app/db/session.py
+++ app/db/session.py
@@ -8,9 +8,15 @@ import os
 # SQLALCHEMY_DB_URL =  "sqlite:///./sql_app.db"
 # SQLALCHEMY_DB_URL = "sqlite+aiosqlite:///./sql_app.db"
 
-DATABASE_URL = f"postgres+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/{os.getenv('POSTGRES_DB')}"
+# DATABASE_URL = f"postgres+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/{os.getenv('POSTGRES_DB')}"
 
-
+DATABASE_URL = (
+    f"postgresql+asyncpg://"
+    f"{os.getenv('POSTGRES_USER')}:"
+    f"{os.getenv('POSTGRES_PASSWORD')}@"
+    f"db:5432/"
+    f"{os.getenv('POSTGRES_DB')}"
+)
 
 
 Base = declarative_base()
