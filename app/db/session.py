35b916f5630477c14822c688c94233b3145bc1ab
Amar Dongardive <adongardive@ACMDC.com>
2025-12-18 16:05:37 +0530

    push to server

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-<<<<<<< HEAD
-
-
-# SQLALCHEMY_DB_URL =  "sqlite:///./sql_app.db"
-SQLALCHEMY_DB_URL = "sqlite+aiosqlite:///./sql_app.db"
-=======
 import os
 
 
@@ -15,20 +9,15 @@ import os
 
 DATABASE_URL = f"postgres+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/{os.getenv('POSTGRES_DB')}"
 
->>>>>>> bf6d81f65e764bc51eda2a5ecaf880e821b80f96
 
 
 
 Base = declarative_base()
 
 # engine = create_async_engine(settings.DATABASE_URL, echo=True)
-<<<<<<< HEAD
-engine = create_async_engine(SQLALCHEMY_DB_URL, echo=True)
-=======
 # engine = create_async_engine(SQLALCHEMY_DB_URL, echo=True)
 engine = create_async_engine(DATABASE_URL, echo=True)
 
->>>>>>> bf6d81f65e764bc51eda2a5ecaf880e821b80f96
 
 
 AsyncSessionLocal = sessionmaker(
