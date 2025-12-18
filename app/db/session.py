bf6d81f65e764bc51eda2a5ecaf880e821b80f96
Amar Dongardive <adongardive@ACMDC.com>
2025-12-18 15:56:52 +0530

    add postgress sql

--- app/db/session.py
+++ app/db/session.py
@@ -1,17 +1,23 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+import os
 
 
 # SQLALCHEMY_DB_URL =  "sqlite:///./sql_app.db"
-SQLALCHEMY_DB_URL = "sqlite+aiosqlite:///./sql_app.db"
+# SQLALCHEMY_DB_URL = "sqlite+aiosqlite:///./sql_app.db"
+
+DATABASE_URL = f"postgres+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/{os.getenv('POSTGRES_DB')}"
+
 
 
 
 Base = declarative_base()
 
 # engine = create_async_engine(settings.DATABASE_URL, echo=True)
-engine = create_async_engine(SQLALCHEMY_DB_URL, echo=True)
+# engine = create_async_engine(SQLALCHEMY_DB_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True)
+
 
 
 AsyncSessionLocal = sessionmaker(
